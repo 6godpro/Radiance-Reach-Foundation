eafcc94a09f059d3b88418f5b18baffa58f075ac
6godpro <erickson5115@gmail.com>
2025-09-17 16:32:01 +0100

    feat(contact-form): add subject field to streamline inbox email organization

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,10 +6,14 @@ import {
   Button,
   Textarea,
   Stack,
+  Field,
+  Portal,
+  Select,
+  createListCollection,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { FaInstagram, FaFacebook } from "react-icons/fa";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { BeatLoader } from "react-spinners";
 import { Mail, Phone } from "lucide-react";
 import { motion } from "framer-motion";
@@ -22,14 +26,39 @@ interface FormValues {
   name: string;
   email: string;
   message: string;
+  title: string;
 }
 
 const MotionButton = motion.create(Button);
 
 // EmailJS environment variables
-const serviceId = import.meta.env.EMAILJS_SERVICE_ID;
-const templateId = import.meta.env.EMAILJS_TEMPLATE_ID;
-const publicKey = import.meta.env.EMAILJS_PUBLIC_KEY;
+const serviceId: string = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const templateId: string = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+const publicKey: string = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+const helpOptions = createListCollection({
+  items: [
+    { label: "Join Volunteer team", value: "volunteer" },
+    { label: "Donation", value: "donation" },
+    { label: "Partner with us", value: "partner" },
+  ],
+});
+
+const subjectMap:Record<string, string> = {
+  volunteer: "Volunteer Application",
+  donation: "Donation Inquiry",
+  partner: "Partnership Request",
+}
+
+const contactFieldStyleProp = {
+  variant: "outline" as const,
+  size: "md" as const,
+  fontSize: "16px",
+  maxW: { base: "", md: "80%" },
+  _focus: { border: "none" },
+  color: "black",
+  bg: "whiteAlpha.700",
+};
 
 const Contact = () => {
   const [loading, setLoading] = useState(false);
@@ -39,21 +68,28 @@ const Contact = () => {
     handleSubmit,
     formState: { errors },
     reset,
+    control,
   } = useForm<FormValues>();
 
   const onSubmit = async (data: FormValues) => {
-    setLoading(!loading);
+    setLoading(true);
     setButtonText("");
+
+    const mappedTitle = subjectMap[data.title]
 
     await emailjs
       .send(
         serviceId,
         templateId,
-        { from_email: data.email, message: data.message },
+        {
+          name: data.name,
+          email: data.email,
+          title: mappedTitle,
+          message: data.message,
+        },
         publicKey
       )
       .then(() => {
-        alert("Message sent");
         setLoading(false);
         setButtonText("Sent");
         reset();
@@ -63,9 +99,9 @@ const Contact = () => {
       });
 
     setTimeout(() => {
+      setLoading(false);
       setButtonText("Send Message");
-    }, 1000);
-    console.log(data);
+    }, 500);
   };
 
   return (
@@ -137,16 +173,17 @@ const Contact = () => {
           <IconText icon={Phone} text="+2348157174593" />
         </Flex>
       </Flex>
+
       {/* Message area */}
       <Box
         as="form"
         onSubmit={handleSubmit(onSubmit)}
         flexBasis="50%"
-        bg="whiteAlpha.500"
+        bg="whiteAlpha.300"
         border="1px rgba(158, 147, 147, 0.2) solid"
         borderRadius="10px"
-        px={{base: 2, md: 5}}
-        py={{base: 2, md: 5}}
+        px={{ base: 2, md: 5 }}
+        py={{ base: 2, md: 5 }}
         mt={{ base: 4 }}
       >
         <Stack gap="4" mt={{ base: "10px", md: "0" }}>
@@ -164,18 +201,13 @@ const Contact = () => {
               required: { value: true, message: "Name is required" },
               minLength: { value: 3, message: "Please enter a valid name" },
             })}
-            variant="outline"
+            {...contactFieldStyleProp}
             placeholder="Name"
-            size="md"
-            fontSize="16px"
-            maxW={{ base: "", md: "80%" }}
             border={
-              errors.email
+              errors.name
                 ? "1px solid red"
                 : "1px rgba(158, 147, 147, 0.2) solid"
             }
-            _focus={{ border: "none" }}
-            color="black"
           />
           {errors.name && (
             <Text fontSize="12px" color="red.500">
@@ -190,40 +222,90 @@ const Contact = () => {
                 message: "Please enter a valid email address",
               },
             })}
-            variant="outline"
+            {...contactFieldStyleProp}
             placeholder="Email"
-            size="md"
-            fontSize="16px"
-            maxW={{ base: "", md: "80%" }}
             border={
               errors.email
                 ? "1px solid red"
                 : "1px rgba(158, 147, 147, 0.2) solid"
             }
-            _focus={{ border: "none" }}
-            color="black"
           />
           {errors.email && (
             <Text fontSize="12px" color="red.500">
               {errors.email.message}
             </Text>
           )}
+          <Field.Root
+            invalid={!!errors.title}
+            width={{ base: "100%", md: "80%" }}
+          >
+            <Field.Label color="gray.600" fontSize="sm">
+              How would you like to help?
+            </Field.Label>
+            <Controller
+              control={control}
+              name="title"
+              rules={{ required: "This field is required" }}
+              render={({ field }) => (
+                <Select.Root
+                  name={field.name}
+                  value={field.value ? [field.value] : []} // convert string -> string[]
+                  onValueChange={(details) =>
+                    field.onChange(details.value[0] ?? "")
+                  }
+                  onInteractOutside={() => field.onBlur()}
+                  collection={helpOptions}
+                  border={
+                    errors.title
+                      ? "0px solid red"
+                      : "0px rgba(158, 147, 147, 0.2) solid"
+                  }
+                  bg="whiteAlpha.700"
+                  rounded="md"
+                  size="lg"
+                >
+                  <Select.HiddenSelect />
+                  <Select.Control>
+                    <Select.Trigger>
+                      <Select.ValueText
+                        placeholder="Choose an option"
+                        fontSize="16px"
+                      />
+                    </Select.Trigger>
+                    <Select.IndicatorGroup>
+                      <Select.Indicator />
+                    </Select.IndicatorGroup>
+                  </Select.Control>
+                  <Portal>
+                    <Select.Positioner>
+                      <Select.Content>
+                        {helpOptions.items.map((item) => (
+                          <Select.Item item={item} key={item.value}>
+                            {item.label}
+                            <Select.ItemIndicator />
+                          </Select.Item>
+                        ))}
+                      </Select.Content>
+                    </Select.Positioner>
+                  </Portal>
+                </Select.Root>
+              )}
+            />
+            <Field.ErrorText>{errors.title?.message}</Field.ErrorText>
+          </Field.Root>
+
+          {/* Message */}
           <Textarea
             {...register("message", { required: "Please enter a message" })}
-            variant="outline"
+            {...contactFieldStyleProp}
             placeholder="Enter your message here"
-            size="md"
-            fontSize="16px"
             minH={{ base: "120px", md: "120px" }}
-            maxW={{ base: "", md: "80%" }}
             border={
-              errors.email
+              errors.message
                 ? "1px solid red"
                 : "1px rgba(158, 147, 147, 0.2) solid"
             }
-            _focus={{ border: "none" }}
             resize="none"
-            color="black"
           />
           {errors.message && (
             <Text fontSize="12px" color="red.500">
