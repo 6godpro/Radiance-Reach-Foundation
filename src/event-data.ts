f4c96ad87ea08f571dd6c764950bf7ecc59e080e
6godpro <erickson5115@gmail.com>
2025-09-23 13:50:50 +0100

    fix: moved all carousel images to public directory

--- src/event-data.ts
+++ src/event-data.ts
@@ -2,10 +2,10 @@ import { client } from "./sanityClient";
 import { SanityEventType } from "./sanityTypes";
 
 export const previewImages = [
-  "/src/assets/december_outreach1.jpg",
-  "/src/assets/december_outreach10.jpg",
-  "/src/assets/december_outreach11.jpg",
-  "/src/assets/december_outreach12.jpg",
+  "/images/december_outreach1.jpg",
+  "/images/december_outreach10.jpg",
+  "/images/december_outreach11.jpg",
+  "/images/december_outreach12.jpg",
 ];
 
 export const fetchEvents = async () => {
