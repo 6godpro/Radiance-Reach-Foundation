890bed26c2a8f2ca5b8c90a96f366d86468a9ac5
6godpro <erickson5115@gmail.com>
2025-11-20 18:25:10 +0100

    feat: Include new components UpcomingEvent and AnnouncementBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,53 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Events from "./components/Events";
+import Events from "./components/EventHighlights";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
 import VideoSection from "./components/VideoPlayer";
 import about from "./assets/about.mp4";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GetInvolved from "./components/GetInvolved";
 import { Toaster } from "./components/ui/toaster";
+import UpcomingEvent from "./components/UpcomingEvent";
+import AnnouncementBar from "./components/AnnouncementBar";
+import { fetchUpcomingEvent } from "./event-data";
+import { SanityUpcomingEventType } from "./sanityTypes";
 
 const App = () => {
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [event, setEvent] = useState<SanityUpcomingEventType | null>(null);
+
+  useEffect(() => {
+    fetchUpcomingEvent()
+      .then((res) => {
+        setEvent(res ?? null);
+      })
+      .catch((err) => {
+        console.error("Error fetching: ", err);
+      });
+  }, []);
+
   return (
     <div style={{ background: "#faf5ef" }}>
       <Navbar />
+      {event?.bannerMessage && (
+        <AnnouncementBar bannerMessage={event.bannerMessage} />
+      )}
       <Hero />
+      <UpcomingEvent event={event} />
       <Events />
-      <About setFullScreen={setIsFullScreen}/>
+      <About setFullScreen={setIsFullScreen} />
       <GetInvolved />
       <Contact />
       <ScrollToTop />
       <Footer />
-      <VideoSection about={about} isFullScreen={isFullScreen} setFullScreen={setIsFullScreen} />
+      <VideoSection
+        about={about}
+        isFullScreen={isFullScreen}
+        setFullScreen={setIsFullScreen}
+      />
       <Toaster />
     </div>
   );
